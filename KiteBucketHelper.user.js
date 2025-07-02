ccdf0385095303dfc2dbe45fedabe703e11d1f29
piyushadlakha <piyushadlakha92@gmail.com>
2025-07-02 23:40:48 +0530

    Update KiteBucketHelper.user.js

--- KiteBucketHelper.user.js
+++ KiteBucketHelper.user.js
@@ -3,7 +3,7 @@
 // @downloadURL  https://github.com/piyushadlakha/GreaseMonkeyScript/raw/refs/heads/main/KiteBucketHelper.user.js
 // @updateURL    https://github.com/piyushadlakha/GreaseMonkeyScript/raw/refs/heads/main/KiteBucketHelper.user.js
 // @namespace    http://tampermonkey.net/
-// @version      5
+// @version      6
 // @description  try to take over the world!
 // @author       You
 // @match        https://kite.zerodha.com/orders/baskets*
